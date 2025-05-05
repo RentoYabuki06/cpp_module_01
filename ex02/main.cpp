1593f97776601dcff5ad3e325aed5a6ce3afaaca
RentoYabuki06 <rento.nky.1203@gmail.com>
2025-05-05 13:14:48 +0900

    ex02 : Update main.cpp to change author information and fix output for string pointer dereferencing

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 20:32:09 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/05/01 21:30:37 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/05/05 13:13:27 by ryabuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int	main()
 	std::string& stringREF = str;
 
 	std::cout << "The memory address of the string variable	 : [" << &str << "]" << std::endl;
-	std::cout << "The memory address held by stringPT		 : [" << &stringPTR << "]" << std::endl;
+	std::cout << "The memory address held by stringPT		 : [" << stringPTR << "]" << std::endl;
 	std::cout << "The memory address held by stringREF		 : [" << &stringREF << "]" << std::endl;
 	std::cout << "--------------------------------------------------------------------" << std::endl;
 	std::cout << "The value of the string variable 		 : [" << str << "]" << std::endl;
-	std::cout << "The value pointed to by stringPTR		 : [" << stringPTR << "]" << std::endl;
+	std::cout << "The value pointed to by stringPTR		 : [" << *stringPTR << "]" << std::endl;
 	std::cout << "The value pointed to by stringREF		 : [" << stringREF << "]" << std::endl;
 	return (0);
 }
